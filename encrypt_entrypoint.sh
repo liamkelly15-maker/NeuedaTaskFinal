0106eb563a6cb253ddc468f44e4cc66171974262
liamkelly15-maker <liamkelly15@gmail.com>
2021-04-19 08:10:50 +0100

    added files

---
+++ encrypt_entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#write a bash to to run in dockerfile
+#use while loop to keep the container active - delete the json file at end and then manually add a new
+#one to see if it is picked up
+
+#cd /
+python encrypt.py
+
+#for debug - should use if [[ -n $DEBUG ]] set an infinite loop to keep the container running
+#if there is a value in the $DEBUG then execute the loop to keep contaner running
+#if [[ -n $DEBUG]]
+#then
+while :
+do
+      sleep 30
+done
+#fi
